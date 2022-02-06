e0b055de78f70bcfc74d25579c32ba39ac4ed074
Marckie Zeender <mkzeender@gmail.com>
2022-02-05 21:28:56 -0800

    added nocturnal mob type and some block types

--- data/mctrlmobs/tags/blocks/tag_generator.py
+++ data/mctrlmobs/tags/blocks/tag_generator.py
@@ -1,9 +1,33 @@
 import json
 
+def write_tag(filename, lst):
+    with open(filename, 'w') as f:
+        json.dump({'values': lst}, f, indent=4)
+
 with open('../../../../blocks.json', 'r') as f:
     blockdata = json.load(f)
 
 
-solid = [block for block in blockdata if block['boundingBox'] == 'block']
+solid = [block['name'] for block in blockdata if block['boundingBox'] == 'block']
 
-permeable = [block for block in blockdata if block['boundingBox'] == 'empty']
+permeable = [block['name'] for block in blockdata if block['boundingBox'] == 'empty']
+
+
+standable_on = []
+for block in solid:
+    if not ('wall' in block and 'wall_' not in block) and not ('fence' in block and 'fence_' not in block):
+        standable_on.append(block)
+
+write_tag('standable_on.json', standable_on)
+
+
+
+
+
+not_standable_in = ['fire', 'soul_fire', 'lava']
+
+standable_in = [x for x in permeable if x not in not_standable_in]
+
+write_tag('standable_in.json', standable_in)
+
+
